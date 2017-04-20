f84493391f25c8d4d5ae1bf0de70bf96e59e9175
avt13003 <andrew.tran@uconn.edu>
2017-04-20 14:29:57 -0400

    TouchID

---
+++ HospitalSystem/EmployeeLoginViewController.swift
@@ -0,0 +1,117 @@
+//
+//  EmployeeLoginViewController.swift
+//  HospitalSystem
+//
+//  Created by Andrew T on 4/20/17.
+//  Copyright © 2017 Andras Palfi. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class EmployeeLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    func notifyUser(_ msg: String, err: String?) {
+        let alert = UIAlertController(title: msg,
+                                      message: err,
+                                      preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "OK",
+                                         style: .cancel, handler: nil)
+        
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true,
+                     completion: nil)
+    }
+    
+    
+    @IBAction func touchIDLogin(_ sender: Any) {
+        
+        let context = LAContext()
+        
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(
+            LAPolicy.deviceOwnerAuthenticationWithBiometrics,
+            error: &error) {
+            
+            // Device can use TouchID
+            context.evaluatePolicy(
+                LAPolicy.deviceOwnerAuthenticationWithBiometrics,
+                localizedReason: "Access requires authentication",
+                reply: {(success, error) in
+                    DispatchQueue.main.async {
+                        
+                        if error != nil {
+                            
+                            switch error!._code {
+                                
+                            case LAError.Code.systemCancel.rawValue:
+                                self.notifyUser("Session cancelled",
+                                                err: error?.localizedDescription)
+                                
+                            case LAError.Code.userCancel.rawValue:
+                                self.notifyUser("Please try again",
+                                                err: error?.localizedDescription)
+                                
+                            case LAError.Code.userFallback.rawValue:
+                                self.notifyUser("Authentication",
+                                                err: "Password option selected")
+                                // Custom code to obtain password here
+                                
+                            default:
+                                self.notifyUser("Authentication failed",
+                                                err: error?.localizedDescription)
+                            }
+                            
+                        } else {
+                            self.notifyUser("Authentication Successful",
+                                            err: "You now have full access")
+                        }
+                    }
+            })
+            
+        } else {
+            // Device cannot use TouchID
+            switch error!.code{
+                
+            case LAError.Code.touchIDNotEnrolled.rawValue:
+                notifyUser("TouchID is not enrolled",
+                           err: error?.localizedDescription)
+                
+            case LAError.Code.passcodeNotSet.rawValue:
+                notifyUser("A passcode has not been set",
+                           err: error?.localizedDescription)
+                
+            default:
+                notifyUser("TouchID not available",
+                           err: error?.localizedDescription)
+                
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
